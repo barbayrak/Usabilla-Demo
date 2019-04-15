520f81de99f6fd74300a86f0fb23d7307a65d94f
Kaan Baris BAYRAK <kaan.bayrak@ozu.edu.tr>
2019-04-15 22:07:06 +0300

    Project Initialized

---
+++ UsabillaAssignment/API/FeedbackService.swift
@@ -0,0 +1,10 @@
+//
+//  FeedbackService.swift
+//  UsabillaAssignment
+//
+//  Created by Kaan Baris BAYRAK on 15.04.2019.
+//  Copyright © 2019 Kaan Baris Bayrak. All rights reserved.
+//
+
+import Foundation
+
